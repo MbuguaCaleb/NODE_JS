07468818a7bfdac59074ff5540bcd128309fc3a7
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-08-21 17:57:31 +0300

    Http Node Js Server

--- logger.js
+++ logger.js
@@ -15,7 +15,7 @@ module.exports = Logger;
 
 let writefile = fs.writeFile(
   path.join(__dirname, "reference/test", "helloWorld.txt"),
-  "Hello World!I have been able to call events in Node JS-Caleb Mbugua Karanja",
+  "Hello World!I have been able to call events in Node JS-Caleb Mbugua Karanja Kerei Ngugi",
   err => {
     if (err) throw err;
 
@@ -31,3 +31,21 @@ class LoggerTwo extends EventEmmiter {
 }
 
 module.exports = LoggerTwo;
+
+//const Logger = require("./logger");
+
+const logger = new Logger();
+
+logger.on("message", data => console.log("Called  Listener", data));
+
+logger.log("Hello World!");
+
+//My Example-MbuguaCaleb
+
+//const LoggerTwo = require("./logger");
+
+const loggerTwo = new LoggerTwo();
+
+loggerTwo.on("message", data => console.log("Called  Listener", data));
+
+loggerTwo.log("Hello World!");
---
+++ notes.js
@@ -0,0 +1,23 @@
+//importing a class
+/*
+Not Yet Implemetned
+
+
+import Person from "./person";
+
+//console.log(__filename, __dirname);
+//Module Wrappernode function
+/*
+(function(exports, require, module, __filename, __dirname) {
+});
+*/
+
+//importing the person class from the file path
+//instanciatig the class
+/*
+const Person = require("./person");
+
+const person1 = new Person("Mbugua Caleb", 30);
+
+person1.greeting();
+*/
